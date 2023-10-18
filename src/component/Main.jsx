5a034b0bb7c40cc610c77d54b8dfea677e7730c9
kailash <kailashrajut0575@gmail.com>
2023-10-18 16:43:44 +0530

    production_start, features/top_section

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -7,8 +7,8 @@ import Clock from "../assets/images/banner_image/Clock.png";
 import Heart from "../assets/images/banner_image/Heart.png";
 import Comment from "../assets/images/top_section_images/Comment.png";
 import banner_img from "../assets/images/banner_image/banner_img.png";
-import style_women from '../assets/images/top_section_images/style_women.png';
-import Card from '../assets/images/top_section_images/Card.png'
+import style_women from "../assets/images/top_section_images/style_women.png";
+import Card from "../assets/images/top_section_images/Card.png";
 const Main = () => {
   return (
     <div className="main_container">
@@ -97,23 +97,36 @@ const Main = () => {
                   <div className="top_left_section_container">
                     <div className="top_left_section">
                       <div className="top_left_top">
-              <div className="top_left_card1">
-<div className="top_left_card1_left">
-<p>Stylish woman in summer outfit isolated posing in fashion trend isolated</p>
-<p>In this captivating scene, a stylish woman stands confidently against a pristine backdrop, capturing the essence of summer's allure. Bathed in the gentle sunlight, she emanates an air of effortless chic. Her outfit is a harmonious blend of contemporary trends and timeless elegance, perfectly attuned to the vibrant season.</p>
-<button type="button" class="btn btn-primary">Read more</button>
-</div>
-<div className="top_left_card1_rigth">
-  <img src={style_women}  className="" alt="" />
-</div>
-              </div>
-               <div className="top_left_card2">
-<div className="card2"><img src={Card} alt="" /></div>
-</div>
+                        <div className="top_left_card1">
+                          <div className="top_left_card1_left">
+                            <p>
+                              Stylish woman in summer outfit isolated posing in
+                              fashion trend isolated
+                            </p>
+                            <p>
+                              In this captivating scene, a stylish woman stands
+                              confidently against a pristine backdrop, capturing
+                              the essence of summer's allure. Bathed in the
+                              gentle sunlight, she emanates an air of effortless
+                              chic. Her outfit is a harmonious blend of
+                              contemporary trends and timeless elegance,
+                              perfectly attuned to the vibrant season.
+                            </p>
+                            <button type="button" class="btn btn-primary">
+                              Read more
+                            </button>
+                          </div>
+                          <div className="top_left_card1_rigth">
+                            <img src={style_women} className="" alt="" />
+                          </div>
+                        </div>
+                        <div className="top_left_card2">
+                          <div className="card2">
+                            <img src={Card} alt="" />
+                          </div>
+                        </div>
                       </div>
-                      <div className="top_left_bottom">
-
-                      </div>
+                      <div className="top_left_bottom"></div>
                     </div>
                   </div>
                 </div>
